0e0663274446af814d7ea2c1b22d3d32653ae308
Matheus Lopes <matheus.lopes23@hotmail.com>
2022-10-29 09:56:15 -0300

    Criado coordinator da tela de listagem dos carros

---
+++ Carangas/Features/Listing/Coordinator/CarsListingCoordinator.swift
@@ -0,0 +1,28 @@
+//
+//  CarsListingCoordinator.swift
+//  Carangas
+//
+//  Created by Matheus Lopes on 29/10/22.
+//
+
+import Foundation
+import UIKit
+
+final class CarsListingCoordinator: Coordinator {
+    var navigationController: UINavigationController
+    
+    var childCoordinators: [Coordinator] = []
+    
+    var parentCoordinator: Coordinator?
+    
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+    
+    func start() {
+        let viewController = CarsTableViewController.instantiateFrom(storyboard: .listing)
+        navigationController.pushViewController(viewController, animated: false)
+    }
+    
+    
+}
